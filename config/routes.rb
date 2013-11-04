5d5707391ffb4a59e2566d518c755beb08e9b6c1
mcarthurgill <mickgill2008@gmail.com>
2013-11-04 14:48:12 -0600

    user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 AptManager::Application.routes.draw do
+
   root to: "users#index"
   resources :users
-
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  resources :sessions
+  
 end
