baedfc2f63dc37c026cde0025f20e1d250f42679
mcarthurgill <mickgill2008@gmail.com>
2013-11-04 14:16:46 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AptManager::Application.config.secret_token = 'e46882a9d8f44c85f178da8af1f39db25f3f1b0d5c8ceb1cc02680ab6ec19a565c4aec8acd80665a613bb794aca27dce45ac3b27d70b7d86651e7af53f68175f'
