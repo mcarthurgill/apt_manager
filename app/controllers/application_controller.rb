8ca2df379545058b42645b4066953c6f14e952b6
mcarthurgill <mickgill2008@gmail.com>
2013-11-04 14:50:37 -0600

    authorization method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,9 @@ class ApplicationController < ActionController::Base
   	def current_user
   		@current_user ||= User.find(session[:user_id]) if session[:user_id]
   	end
-
   	helper_method :current_user
+
+  	def authorize
+  		redirect_to login_url, alert: "Not Authorized" if current_user.nil?
+  	end
 end
