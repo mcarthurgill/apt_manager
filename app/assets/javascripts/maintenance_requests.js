e27b28262311ec33a1a1e475d776e3fb573dd863
mcarthurgill <mickgill2008@gmail.com>
2013-11-13 13:46:10 -0600

    working arrows

--- app/assets/javascripts/maintenance_requests.js
+++ app/assets/javascripts/maintenance_requests.js
@@ -1,4 +1,14 @@
 $(function(){
   $(".tablesorter-blue").tablesorter({ sortList: [1,0] });
+
+  $("thead .headerSortDown").on("click", function(){
+  	$(this).toggleClass("headerSortDown");
+  	$(this).toggleClass("headerSortUp");
+  });
+
+  $("thead .headerSortUp").on("click", function(){
+  		$(this).toggleClass("headerSortUp");
+  		$(this).toggleClass("headerSortDown");
+  });
 });
 
