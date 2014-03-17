46ce647400cc972bb74363fd1d876e754fa1bdf0
mcarthurgill <mickgill2008@gmail.com>
2014-03-17 15:54:38 -0500

    delete from cloudinary

--- app/models/bg_image.rb
+++ app/models/bg_image.rb
@@ -4,4 +4,17 @@ class BgImage < ActiveRecord::Base
   belongs_to :dummy
 
   mount_uploader :file, PictureUploader
+
+  before_destroy :delete_from_cloudinary
+
+  private
+
+  def delete_from_cloudinary   
+  	p "*"*50
+  	p "deleting from cloudinary..."
+    public_id = self.file.model.file.identifier.split("/").last.split(".").first
+    p "public_id: #{public_id}"
+    p "*"*50  	
+    Cloudinary::Uploader.destroy(public_id)
+  end 
 end
